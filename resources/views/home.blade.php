b6cf7ee15ea90534fa6766689125fc35b30d9970
MikaOcko <moreau.magali.kimberly@gmail.com>
2023-03-13 20:09:10 +0100

    modification de la page d'accueil

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,31 +7,4 @@
         </h2>
         <p>Un blog codé avec Laravel 10</p>
     </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 text-center">
-                    {{ __("Mangeur de couscous") }}
-                    
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-3">
-            <h1 class="text-center font-semibold text-xl"> Mangeur de couscous</h1>
-        </div>
-    </div>
-
-    <x-danger-button/>
-    <x-primary-button>
-        <p>Lorem ipsum dolor sit amet.</p>
-    </x-primary-button>
-    <x-secondary-button>
-        <p>Lorem ipsum dolor sit amet.</p>
-    </x-secondary-button>
-    
-
 </x-app-layout>
